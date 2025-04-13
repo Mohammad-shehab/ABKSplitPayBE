c4bd3b3fba6382fc5ecefaf62281910249c296c4
Mohammad-shehab <your.q8mlg@outlook.com>
2025-04-13 11:13:35 +0300

    Fixed Url

--- ABKSplitPayBE/Data/ApplicationDbContext.cs
+++ ABKSplitPayBE/Data/ApplicationDbContext.cs
@@ -253,7 +253,7 @@ namespace ABKSplitPayBE.Data
                     StockQuantity = 0, // Unlimited for bills
                     ProductCategoryId = 2,
                     StoreId = 5, // American International School
-                    PictureUrl = "https://www.ais-kuwait.org/admissions/tuition-fees/",
+                    PictureUrl = "https://media.licdn.com/dms/image/v2/C561BAQE_m2Ujim4lyQ/company-background_10000/company-background_10000/0/1584559760344/american_international_school_kuwait_cover?e=2147483647&v=beta&t=zt2lRU5uJA-o2CHmJuVDqoWV8SIqa_W_F69eqQ3yRhs",
                     IsActive = true
                 },
                 new Product
@@ -265,7 +265,7 @@ namespace ABKSplitPayBE.Data
                     StockQuantity = 100,
                     ProductCategoryId = 2,
                     StoreId = 6, // AUM
-                    PictureUrl = "https://www.aum.edu.kw/english/admission/undergraduate-admission/tuition-fees",
+                    PictureUrl = "https://www.aum.edu.kw/images/ShareLogo.jpg",
                     IsActive = true
                 },
                 // Medical Expenses
